268d254aca67b7248e9e940a3708f1dc57bf0a1d
sree hk <hk2261080@gmail.com>
2023-12-07 07:08:38 +0000

    post

--- models/Staging/STG_ACCOUNTS.sql
+++ models/Staging/STG_ACCOUNTS.sql
@@ -2,7 +2,9 @@
         config(
         materialized="table",
         enabled=true,
-        schema = 'Staging'
+        schema = 'Staging',
+        post_hook="Update {{ env_var('DBT_GCP_PROJECT') }}.Landing.INFOFISCUS_CT CT SET LAST_RUN_DATE = CURRENT_TIMESTAMP() WHERE
+        SRC_OBJECT =  'ACCOUNTS' AND TGT_OBJECT = 'STG_ACCOUNTS' ;"
     
         ) 
 }}
